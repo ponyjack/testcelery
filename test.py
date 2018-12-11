05d82745a374f97a612694a35b6f2a9ad5c4a782
cheng zhou <chengz.com@gmail.com>
2018-12-11 14:27:30 +0800

    add log

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ from lic.loadtask.slavetasks import StartSpaw
 
 print "i am strat"
 while True:
-    s = StartSpaw.apply_async((10,))
+    s = StartSpaw.apply_async((1,))
 print(s)
 print "i am end"
