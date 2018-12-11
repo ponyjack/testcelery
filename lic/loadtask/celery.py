75442654ef46afcf27f80cc2cb9aaf09b6e18710
cheng zhou <chengz.com@gmail.com>
2018-12-11 10:52:21 +0800

    add log

--- lic/loadtask/celery.py
+++ lic/loadtask/celery.py
@@ -4,9 +4,9 @@ import os
 
 app = Celery(include=[ "lic.loadtask.slavetasks"])
 
-# if not app.conf.broker_url:
-#     app.conf.broker_url = os.getenv("broker_url")
-#     app.conf.result_backend = os.getenv("result_backend")
-#     if not app.conf.broker_url:
-#         if not app.config_from_envvar("LICCONFIG", silent=True):
-#             app.config_from_object("licconfig")
+if not app.conf.broker_url:
+    app.conf.broker_url = os.getenv("broker_url")
+    app.conf.result_backend = os.getenv("result_backend")
+    if not app.conf.broker_url:
+        if not app.config_from_envvar("LICCONFIG", silent=True):
+            app.config_from_object("licconfig")
