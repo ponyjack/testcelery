e2b3104ee4099051b1eac62f06fd91cb68751976
cheng zhou <chengz.com@gmail.com>
2018-12-13 14:51:57 +0800

    add log

--- test.py
+++ test.py
@@ -2,14 +2,18 @@ from lic.loadtask.slavetasks import StartSpaw
 import sys
 
 
-def main(count):
+def main(count, queue=celery):
     print "i am strat"
     for _ in range(count):
-        s = StartSpaw.apply_async((1,))
+        s = StartSpaw.apply_async((1,),queue=queue)
     print(s)
     print "i am end"
 
 
 if __name__ == "__main__":
-    main(int(sys.argv[1]))
+    if len(sys.argv) ==3:
+        main(int(sys.argv[1]),sys.argv[2])
+    
+    else:
+        main(int(sys.argv[1]))
 
