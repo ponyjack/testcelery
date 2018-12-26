fa262b8119273d450cc224e0bf6936dcf82444a5
cheng zhou <chengz.com@gmail.com>
2018-12-26 23:36:38 +0800

    add log

--- lic/loadtask/celery.py
+++ lic/loadtask/celery.py
@@ -1,10 +1,11 @@
 from __future__ import absolute_import
 from celery import Celery
 import os
-
+import logging
 app = Celery(include=[ "lic.loadtask.slavetasks"])
 
 if not app.conf.broker_url:
+    logging.info("sdfsdfsfsfsdfsfsfd")
     app.conf.broker_url = os.getenv("broker_url")
     app.conf.result_backend = os.getenv("result_backend")
     if not app.conf.broker_url:
