083662bd1f093b77d078e87f57ab9c30a23c7236
cheng zhou <chengz.com@gmail.com>
2018-12-26 23:45:01 +0800

    add log

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 docker stop testcelery
 docker rm testcelery
 git pull
-docker run  --name testcelery -v "$(pwd)":/home/testcelery --env CELERY_QUEUE=1111 celery  celery -A lic.loadtask worker --loglevel=debug   -P gevent -c 1  --without-gossip --without-mingle --without-heartbeat --config=licconfig
+docker run  --name testcelery -v "$(pwd)":/home/testcelery --env broker_url=redis://10.11.65.34:6379/1 celery  celery -A lic.loadtask worker --loglevel=debug   -P gevent -c 1  --without-gossip --without-mingle --without-heartbeat --config=licconfig
 # docker run  -d  -v /home/testcelery:/home/testcelery  testcelery
 # docker run  --name testcelery -v "$(pwd)":/home/testcelery --env CELERY_QUEUE=1111 celery
 
