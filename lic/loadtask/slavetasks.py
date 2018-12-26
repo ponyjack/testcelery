2238290d7cb0e6cad453f898445280eb10934971
cheng zhou <chengz.com@gmail.com>
2018-12-26 20:15:16 +0800

    add log

--- lic/loadtask/slavetasks.py
+++ lic/loadtask/slavetasks.py
@@ -2,26 +2,23 @@ from lic.loadtask.celery import app
 import os
 import gevent 
 import time
-from multiprocessing import Pool
 
 
-pool = Pool(processes=1)
 
 
-def forLog(dursion):
-    now  = time.time()
-    while (time.time() - now<dursion):
-        print "xxx"
-        break
+
+
 
 
 @app.task()
 def StartSpaw(xxx):
+    print("Ddfsdfsdfsd")
+
     # gevent.sleep(0)
     # for _ in range(20):
     #     gevent.spawn(forLog, xxx)
     # gevent.sleep(0)
 #     time.sleep(20)
-    forLog(xxx)
+#     forLog(xxx)
 
 
